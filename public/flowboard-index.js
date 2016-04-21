01320f9419d4ef7f3c8e4a5937656b70045600a3
daemon <0x8badc0de@gmail.com>
2016-04-21 02:08:30 -0400

    Add sign up and login UI

--- public/flowboard-index.js
+++ public/flowboard-index.js
@@ -6,10 +6,51 @@ $(document).mouseup(function() {
     expanded = false;
   }
 });
+
 $("#topic-title").click(function() {
   if (!expanded)
   {
     $("#topic-form").animate({"height": "195px"}, "fast");
     expanded = true;
+  }
+});
+
+var loginExpanded = false;
+var signupExpanded = false;
+$("#login-btn").click(function() {
+  $(".bg-cover").css("visibility", "visible");
+  $(".bg-cover").animate({"opacity": 0.4}, "slow");
+  $("#login-container").css("visibility", "visible");
+  $("#login-container").animate({"opacity": 1}, "slow");
+  $("#login-user").focus();
+  loginExpanded = true;
+});
+
+$("#signup-btn").click(function() {
+  $(".bg-cover").css("visibility", "visible");
+  $(".bg-cover").animate({"opacity": 0.4}, "slow");
+  $("#signup-container").css("visibility", "visible");
+  $("#signup-container").animate({"opacity": 1}, "slow");
+  $("#signup-email").focus();
+  signupExpanded = true;
+});
+
+$(".bg-cover").click(function() {
+  if (loginExpanded) {
+    $(".bg-cover").css("opacity", 0);
+    $(".bg-cover").css("visibility", "hidden");
+    $("#login-container").css("visibility", "hidden");
+    $("#login-container").css("opacity", 0);
+    $("#login-container input").val("");
+    $("#login-container input[type=submit]").val("Login");
+    loginExpanded = false
+  } else if (signupExpanded) {
+    $(".bg-cover").css("opacity", 0);
+    $(".bg-cover").css("visibility", "hidden");
+    $("#signup-container").css("visibility", "hidden");
+    $("#signup-container").css("opacity", 0);
+    $("#signup-container input").val("");
+    $("#signup-container input[type=submit]").val("Sign up");
+    signupExpanded = false
   }
 });
