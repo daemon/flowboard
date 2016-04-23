29abead8473b4625a30b0dc6583d09a4b0ef2c8f
daemon <0x8badc0de@gmail.com>
2016-04-22 20:23:09 -0400

    Finish registration UI

--- flowboard.py
+++ flowboard.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+from autobahn.twisted.websocket import WebSocketServerFactory, WebSocketServerProtocol, listenWS
 import cgi
 import cherrypy
 import flowboard_auth
+import json
 import os
 import os.path
 import pymongo
+import sys
+import threading
+from twisted.internet import reactor, ssl
+from twisted.python import log
 
 """
 Router for all endpoints. All ReST API endpoints return JSON.
@@ -13,6 +19,7 @@ class FlowBoard:
   def __init__(self):
     client = pymongo.MongoClient()
     self.auth_service = flowboard_auth.AuthService(flowboard_auth.AuthDatabase(client))
+    FlowBoard.instance = self
 
   """
   / endpoint
@@ -22,7 +29,7 @@ class FlowBoard:
     return open('index.html')
 
   """
-  /register endpoint.
+  /register endpoint
     @param user the username to register. It MUST be at least 2 printable characters long, trailing and leading whitespace excepted.
     @param password the password to use. It MUST be at least 10 characters long with an alphabet of upper, lower, and numeric characters.
     @param email the email to use
@@ -35,7 +42,22 @@ class FlowBoard:
     form = flowboard_auth.AuthService.FormData(user, password, email)
     return self.auth_service.register(form)
 
+class FlowBoardProtocol(WebSocketServerProtocol):
+  def onMessage(self, payload, isBinary):
+    json_request = json.loads(payload.decode("utf8"))
+    json_response = FlowBoard.instance.register(json_request['user'], json_request['password'], json_request['email'])
+    self.sendMessage(json.dumps(json_response).encode())
+
 if __name__ == '__main__':
+  '''Websocket server'''
+  log.startLogging(sys.stdout)
+  ctxt_factory = ssl.DefaultOpenSSLContextFactory('tls/privkey.pem', 'tls/fullchain.pem')
+  #ctxt_factory._context.use_certificate_chain_file('tls/fullchain.pem')
+  factory = WebSocketServerFactory("wss://192.198.93.218:9001")
+  factory.protocol = FlowBoardProtocol
+  listenWS(factory, ctxt_factory)
+
+  '''CherryPy server'''
   conf = {
     '/': {
       'tools.sessions.on': True,
@@ -47,5 +69,8 @@ if __name__ == '__main__':
     }
   }
 
-cherrypy.server.socket_host = '127.0.0.1'
-cherrypy.quickstart(FlowBoard(), '/', conf)
+  cherrypy.server.socket_host = '127.0.0.1'
+  th = threading.Thread(target=reactor.run, kwargs={"installSignalHandlers": 0})
+  th.start()
+  cherrypy.quickstart(FlowBoard(), '/', conf)
+  th.join()
--- public/flowboard-index.js
+++ public/flowboard-index.js
@@ -19,38 +19,92 @@ var loginExpanded = false;
 var signupExpanded = false;
 $("#login-btn").click(function() {
   $(".bg-cover").css("visibility", "visible");
-  $(".bg-cover").animate({"opacity": 0.4}, "slow");
+  $(".bg-cover").fadeTo(500, 0.4);
   $("#login-container").css("visibility", "visible");
-  $("#login-container").animate({"opacity": 1}, "slow");
+  $("#login-container").fadeTo(500, 1);
   $("#login-user").focus();
   loginExpanded = true;
 });
 
 $("#signup-btn").click(function() {
   $(".bg-cover").css("visibility", "visible");
-  $(".bg-cover").animate({"opacity": 0.4}, "slow");
+  $(".bg-cover").fadeTo(500, 0.4);
   $("#signup-container").css("visibility", "visible");
-  $("#signup-container").animate({"opacity": 1}, "slow");
+  $("#signup-container").fadeTo(500, 1);
   $("#signup-email").focus();
   signupExpanded = true;
 });
 
 $(".bg-cover").click(function() {
   if (loginExpanded) {
-    $(".bg-cover").css("opacity", 0);
-    $(".bg-cover").css("visibility", "hidden");
-    $("#login-container").css("visibility", "hidden");
-    $("#login-container").css("opacity", 0);
+    $(".bg-cover").fadeOut();
+    $("#login-container").fadeOut();
     $("#login-container input").val("");
     $("#login-container input[type=submit]").val("Login");
     loginExpanded = false
   } else if (signupExpanded) {
-    $(".bg-cover").css("opacity", 0);
-    $(".bg-cover").css("visibility", "hidden");
-    $("#signup-container").css("visibility", "hidden");
-    $("#signup-container").css("opacity", 0);
+    $(".bg-cover").fadeOut();
+    $("#signup-container").fadeOut();
     $("#signup-container input").val("");
     $("#signup-container input[type=submit]").val("Sign up");
     signupExpanded = false
   }
 });
+
+var signupErrorTxt = "<span style='display: block; width: 100%; text-align: center;'>";
+
+var cssFormValidate = function(formElement, valid) {
+  if (valid)
+    $(formElement).css("border-bottom", "1px solid #AAA");
+  else
+    $(formElement).css("border-bottom", "1px solid red");
+};
+
+var doSignup = function() {
+  var socket = new WebSocket("wss://flowboard.rocketeer.net:9001");
+  var email = $("#signup-email").val()
+  var user = $("#signup-user").val()
+  var password = $("#signup-password").val()
+  var recaptchaResponse = $("#g-recaptcha-response").val();
+  var signupForm = $("#signup-form");  
+  signupForm.hide();
+  cssFormValidate("#signup-password", true);
+  cssFormValidate("#signup-email", true);
+  cssFormValidate("#signup-user", true);
+  var signupErrorNotify = $("#signup-error-notify");
+  signupErrorNotify.html(signupErrorTxt + "Signing up...</span>");
+  socket.onmessage = function(event) {
+    response = JSON.parse(event.data);
+    msg = "";
+    socket.close();
+    signupErrorNotify.html("");
+    signupForm.show();
+    if (response.hasOwnProperty("password_valid")) {
+      cssFormValidate("#signup-password", response["password_valid"]);
+      cssFormValidate("#signup-email", response["email_valid"]);
+      cssFormValidate("#signup-user", response["user_valid"]);
+      if (!response["password_valid"])
+        msg += "Password needs to be at least 10 characters long with numbers, lowercase, and uppercase. ";
+      if (!response["user_valid"])
+        msg += "Username must be at least 2 printable characters long.";
+    } else if (response.hasOwnProperty("name_dup")) {
+      if (response["name_dup"] || response["email_dup"])
+        msg = "Name and/or email are already taken. ";
+      cssFormValidate("#signup-email", !response["email_dup"]);
+      cssFormValidate("#signup-user", !response["name_dup"]);
+    }
+    if (msg !== "")
+      signupErrorNotify.html(signupErrorTxt + msg + "</span>");
+    else if (response["success"])
+    {
+      signupForm.hide();
+      signupErrorNotify.html(signupErrorTxt + "Success!</span>");
+      window.setTimeout(function() {$(".bg-cover").click()}, 2500);
+    }
+  }
+
+  socket.onopen = function(e) {
+    socket.send(JSON.stringify({'email': email, 'user': user, 'password': password, 'recaptcha_response': recaptchaResponse}));
+  }
+  return false;
+};
